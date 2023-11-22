85eadfbb5f3666d5a41fca7d029259409a7ad76c
stappost <emanuele.scarpantoni@gmail.com>
2023-11-22 09:07:45 +0100

    recap finale in html

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const button = document.querySelector(".btn");
 // EVENTO BUTTON VARIABILI KM ETA E PREZZO TOTALE 
 
 button.addEventListener("click", function(){
+    let name = document.getElementById("nome").value;
+    let surname = document.getElementById("cognome").value;
     let km = document.getElementById("km").value;
     let eta = document.getElementById("eta").value;
     let prezzo_totale = km * 0.21;
@@ -19,5 +21,13 @@ button.addEventListener("click", function(){
     else{
         prezzo_totale = prezzo_totale.toFixed(2);
     }
-    console.log(prezzo_totale)
+    console.log(prezzo_totale);
+    document.getElementById("recap").innerText = `Benvenuto ${name} ${surname}. Controllare prima di confermare l'acquisto:
+    km da percorrere = ${km}
+    età = ${eta}
+    prezzo totale = ${prezzo_totale}`
 })
+
+
+
+
